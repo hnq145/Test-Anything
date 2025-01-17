1b3a01872b53302d8287a726ebdc8933c26298a7
Hoang Ngoc Quy <quyhoang1452003@gmail.com>
2025-01-17 14:51:41 +0700

    Thêm folder mới vào dự án

---
+++ Python-Django/.history/mysite/news/views_20250116101352.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the news index 1234.")
+
+def index123(request):
+    return HttpResponse("Hello, world. You're at the news index 1234.")
---
+++ Python-Django/mysite/news/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the news index.")
+
+def index123(request):
+    return HttpResponse("Hello, world. You're at the news index 123.")
